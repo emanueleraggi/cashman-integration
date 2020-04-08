7a57665ee3ca251cf544d9406d7f99676e668d1c
emanuele <emanuele@system76-pc.localdomain>
2020-04-08 18:28:24 -0400

    changed port to 3000

--- server/app.js
+++ server/app.js
@@ -68,7 +68,8 @@ app.use(function(err, req, res, next) {
 	res.render('error');
 });
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
